6e313b8d7e9672a931e294f94788439465f84438
lucho97dilo <ldilorenzo46@gmail.com>
2023-06-12 00:56:22 -0300

    subo ejercicio terminado

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var edad = prompt ("Por favor ingrese su edad")
+
+if (edad >=18){
+    alert ("Bienvenido")
+}else{
+    alert ("No puede ingresar , usted es menor de edad ")
+}
---
+++ js2/main2.js
@@ -0,0 +1,12 @@
+
+
+var usuario = prompt ("Ingrese su usuario")
+var contraseña = prompt ("ingrese su contraseña")
+
+if(usuario== "admi" && contraseña == "1234")
+{
+alert("¡Bienvenido!")
+}else{ 
+alert("Su usuario o contraseña es incorrecta")
+}
+
