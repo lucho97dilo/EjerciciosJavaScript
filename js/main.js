2d9d1028a498b38c646007ffbd1b4a4d01f1249e
lucho97dilo <ldilorenzo46@gmail.com>
2023-06-12 00:20:02 -0300

    subo ejerc terminado

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+var costoFijo = prompt("Ingrese su costo fijo")
+var venta = prompt ("Ingrese su precio de venta")
+var costoVariable = prompt ("Ingrese su costo variable")
+var resultado = costoFijo / (venta - costoVariable ) ;
+alert("Su punto de equilibro es "  +  resultado ) 
+
+
---
+++ js2/main2.js
@@ -0,0 +1,16 @@
+
+const modelo50 = 50
+const modelo30 = 30
+const modelo20 = 20
+const divisor = 100
+
+var presupuesto = prompt ("Ingrese su presupuesto")
+
+var resultado = presupuesto * modelo50 / divisor
+alert("Sus gastos necesarios son " + resultado );
+
+var resultado2 = presupuesto * modelo30 / divisor
+alert("Sus gastos opcionales son " + resultado2 );
+
+var resultado3 = presupuesto * modelo20 / divisor
+alert("Sus ahorro e inversión es " + resultado3); 
